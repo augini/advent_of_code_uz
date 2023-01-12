fb5e68897be9fb9b8b8339cd1c765cf16b6c2b3a
tokhirjon <tokhirjon007@gmail.com>
2023-01-12 14:55:53 +0500

    minor changes

--- 2022/Java/Tokhirjon/day_02/Day2.java
+++ 2022/Java/Tokhirjon/day_02/Day2.java
@@ -6,11 +6,11 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Day2 {
-    public int day2_1() throws IOException {
+    public int problem1() throws IOException {
         return solution(1);
     }
 
-    public int day2_2() throws IOException {
+    public int problem2() throws IOException {
         return solution(2);
     }
 
--- 2022/Java/Tokhirjon/day_02/Main.java
+++ 2022/Java/Tokhirjon/day_02/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Day2 day2 = new Day2();
-        System.out.println(day2.day2_1());
-        System.out.println(day2.day2_2());
+        System.out.println(day2.problem1());
+        System.out.println(day2.problem2());
     }
 }
