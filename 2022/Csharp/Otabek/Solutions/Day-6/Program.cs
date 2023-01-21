25a0c10901d173ef3b5c2813d3f399424044f6b9
Otabek Kholmirzaev <otabekmirzo@outlook.com>
2023-01-22 04:02:57 +0500

    Solved day-6

---
+++ 2022/Csharp/Otabek/Solutions/Day-6/Program.cs
@@ -0,0 +1,36 @@
+﻿var input = File.ReadAllText("Input.txt");
+
+Console.WriteLine("Part one answer: " + FindMarkerPosition(input, 4));
+Console.WriteLine("Part two answer: " + FindMarkerPosition(input, 14));
+
+int FindMarkerPosition(string input, int distCharsCount)
+{
+    var ans = -1;
+    for (int i = distCharsCount - 1; i < input.Length; i++)
+    {
+        var dic = new Dictionary<char, bool>();
+
+        var IsNonRepeating = true;
+
+        for (int j = i - distCharsCount + 1; j <= i; j++)
+        {
+            if (dic.ContainsKey(input[j]))
+            {
+                IsNonRepeating = false;
+                break;
+            }
+            else
+            {
+                dic.Add(input[j], true);
+            }
+        }
+
+        if (IsNonRepeating)
+        {
+            ans = i + 1;
+            break;
+        }
+    }
+
+    return ans;
+}
