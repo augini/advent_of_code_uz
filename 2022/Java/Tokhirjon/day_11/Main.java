a72dda20fb16ea33a4a767a9437b79f7b140f6fd
tokhirjon <tokhirjon007@gmail.com>
2023-02-16 14:42:20 +0500

    day 12 problems were solved

--- 2022/Java/Tokhirjon/day_11/Main.java
+++ 2022/Java/Tokhirjon/day_11/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Day11 day11 = new Day11();
-//        day11.day11_1();
-        day11.day11_2();
+        day11.day11_1();
+//        day11.day11_2();
     }
 }
