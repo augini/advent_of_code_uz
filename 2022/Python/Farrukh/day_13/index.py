251b1b140433e16679be52644c7c29a06a5c79c2
Atabekov Farrukh <augini@inbox.ru>
2023-02-19 11:12:37 +0500

    feat: solve part 2

--- 2022/Python/Farrukh/day_13/index.py
+++ 2022/Python/Farrukh/day_13/index.py
@@ -29,6 +29,7 @@ def check(left, right):
     elif isinstance(left, list) and isinstance(right, int):
         return check(left, [right])
     else:
+        # TODO: checkout the implementation with zip
         l1, l2 = len(left), len(right)
         count = 0
         while count < l1 and count < l2:
@@ -48,18 +49,61 @@ def check(left, right):
 def part1(data):
     """Solve part 1."""
     l = len(data)
-    count = 0
+    _sum = 0
 
     for i in range(0, l, 3):
         left, right = eval(data[i]), eval(data[i + 1])
         res = check(left, right)
         if res == 1:
-            count += (i // 3) + 1
-    return count
+            _sum += (i // 3) + 1
+    return _sum
+
+
+# Space Complexity:
+# Time Complexity:
 
 
 def part2(data):
     """Solve part 2."""
+    l = len(data)
+    _sum = 0
+    ordered = []
+    i1, i2 = 0, 0
+    # append items that come before [[2]]
+    for item in data:
+        if item.strip() == "":
+            continue
+        left, right = eval(item), [[2]]
+        res = check(left, right)
+        if res == 1:
+            ordered.append(eval(item))
+
+    ordered.append([[2]])
+    i1 = len(ordered)
+
+    # append items that come after [[2]] and before [[6]]
+    for item in data:
+        if item.strip() == "":
+            continue
+        _res = check([[2]], eval(item))
+        res = check(eval(item), [[6]])
+
+        if res == 1 and _res == 1:
+            ordered.append(eval(item))
+
+    ordered.append([[6]])
+    i2 = len(ordered)
+
+    # append items that come after [[6]]
+    for item in data:
+        if item.strip() == "":
+            continue
+        res = check([[6]], eval(item))
+
+        if res == 1:
+            ordered.append(eval(item))
+
+    return i1 * i2
 
 
 def solve(puzzle_input):
