0b2f04bfbef1494d89e764343d2d80f5d294b581
Atabekov Farrukh <augini@inbox.ru>
2023-02-19 11:37:29 +0500

    fix: update day 13 part 2

--- 2022/Python/Farrukh/day_13/index.py
+++ 2022/Python/Farrukh/day_13/index.py
@@ -65,45 +65,36 @@ def part1(data):
 
 def part2(data):
     """Solve part 2."""
-    l = len(data)
-    _sum = 0
-    ordered = []
-    i1, i2 = 0, 0
+    i1, i2 = 1, 1
+    data = list(filter(lambda x: x, data))
+
     # append items that come before [[2]]
     for item in data:
-        if item.strip() == "":
-            continue
-        left, right = eval(item), [[2]]
-        res = check(left, right)
-        if res == 1:
-            ordered.append(eval(item))
+        # get lines that are less than [[2]] only
+        res_two = check(eval(item), [[2]])
 
-    ordered.append([[2]])
-    i1 = len(ordered)
+        # get lines that are less [[6]]
+        res_six = check(eval(item), [[6]])
+
+        if res_six == 1:
+            i2 += 1
+        if res_two == 1:
+            i1 += 1
+
+    # length += 1
+    # i1 = length
 
     # append items that come after [[2]] and before [[6]]
-    for item in data:
-        if item.strip() == "":
-            continue
-        _res = check([[2]], eval(item))
-        res = check(eval(item), [[6]])
+    # for item in data:
+    #     _res = check([[2]], eval(item))
+    #     res = check(eval(item), [[6]])
+    #     if res == 1 and _res == 1:
+    #         length += 1
 
-        if res == 1 and _res == 1:
-            ordered.append(eval(item))
+    # length += 1
+    # i2 = length
 
-    ordered.append([[6]])
-    i2 = len(ordered)
-
-    # append items that come after [[6]]
-    for item in data:
-        if item.strip() == "":
-            continue
-        res = check([[6]], eval(item))
-
-        if res == 1:
-            ordered.append(eval(item))
-
-    return i1 * i2
+    return i1 * (i2 + 1)
 
 
 def solve(puzzle_input):
