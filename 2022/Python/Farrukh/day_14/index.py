05ce6b491c4153e01b8d787cb16dc24a87d8df58
Atabekov Farrukh <augini@inbox.ru>
2023-02-26 08:37:38 +0500

    solve: day 14

--- 2022/Python/Farrukh/day_14/index.py
+++ 2022/Python/Farrukh/day_14/index.py
@@ -3,6 +3,8 @@
 import pathlib
 import sys
 from collections import defaultdict
+import math
+
 
 def parse(puzzle_input, type=1, seperator="\n"):
     # Parses the input
@@ -16,73 +18,121 @@ def parse(puzzle_input, type=1, seperator="\n"):
         return list(puzzle_input.strip().split(seperator))
 
 
-def part1(data):
-    """Solve part 1."""
-    # create the lines that include rocks
-    rocks = defaultdict(set)
+def get_rocks(coors):
+    rocks = set()
 
-    for item in data:
+    for item in coors:
         parts = item.split("->")
         prev = parts[0]
-        
+
         for i in range(1, len(parts)):
-            prev, curr = prev.strip().split(","), parts[i].strip().split(",")     
-            
+            prev, curr = prev.strip().split(","), parts[i].strip().split(",")
+
             for x in range(2):
                 prev[x], curr[x] = int(prev[x]), int(curr[x])
-            
+
             x1, y1 = prev
             x2, y2 = curr
 
             if x1 == x2:
                 _min = min(y1, y2)
                 _max = max(y1, y2)
-                
-                for y in range(_min, _max+1):
-                    rocks[x1].add(y)
+
+                for y in range(_min, _max + 1):
+                    rocks.add((x1, y))
 
             if y1 == y2:
                 _min = min(x1, x2)
                 _max = max(x1, x2)
-                
-                for y in range(_min , _max):    
-                    rocks[y].add(y1) 
-            prev = parts[i]
-    # print(rocks)
-    count = 0
-    sands = defaultdict(set)
 
-    while count < 23:
-        count+=1
-        sx, sy = 500, 0
+                for y in range(_min, _max + 1):
+                    rocks.add((y, y1))
+            prev = parts[i]
+    return rocks
+
+
+def part1(data):
+    """Solve part 1."""
+    # create the lines that include rocks
+    rocks = get_rocks(data)
+
+    # get the bottom of the floow as the row that has the lowest coor
+    bottom = max(r[1] for r in rocks)
+
+    sands, result = set(), 0
+    count = 0
+
+    while count < 1000:
+        count += 1
+        sand = (500, 0)
 
         while True:
-            if not sy+1 in rocks[sx] and not sy+1 in sands[sx]:
-                sy+=1
-                continue
-            elif not sy+1 in rocks[sx-1] and not sy+1 in sands[sx-1]:
-                sx-=1
-                sy+=1
-                print(sx, sy)
-                continue
-            elif not sy+1 in rocks[sx+1] and not sy+1 in sands[sx+1]:
-                sx+=1
-                sy+=1
-                continue
+            if sand[1] > bottom and not result:
+                return count
+            if (sand[0], sand[1] + 1) not in rocks and (
+                sand[0],
+                sand[1] + 1,
+            ) not in sands:
+                sand = (sand[0], sand[1] + 1)
+            elif (sand[0] - 1, sand[1] + 1) not in rocks and (
+                sand[0] - 1,
+                sand[1] + 1,
+            ) not in sands:
+                sand = (sand[0] - 1, sand[1] + 1)
+            elif (sand[0] + 1, sand[1] + 1) not in rocks and (
+                sand[0] + 1,
+                sand[1] + 1,
+            ) not in sands:
+                sand = (sand[0] + 1, sand[1] + 1)
             else:
-                sands[sx].add(sy)
                 break
-        if (sx, sy) == (500, 0):
-            break
+        sands.add(sand)
 
-    sm = 0
-    for v in sands.values():
-        sm+=len(v)
-    return sm
+    return 1
 
 
 def part2(data):
     """Solve part 2."""
+    # create the lines that include rocks
+    rocks = get_rocks(data)
+
+    # get the bottom of the floow as the row that has the lowest coor
+    bottom = max(r[1] for r in rocks) + 2
+
+    left = min(r[0] for r in rocks) - 500
+    right = max(r[0] for r in rocks) + 500
+
+    for x in range(left, right):
+        rocks.add((x, bottom))
+
+    sands = set()
+    count = 0
+    while count < 100000:
+        count += 1
+        sand = (500, 0)
+        while True:
+            if (sand[0], sand[1] + 1) not in rocks and (
+                sand[0],
+                sand[1] + 1,
+            ) not in sands:
+                sand = (sand[0], sand[1] + 1)
+            elif (sand[0] - 1, sand[1] + 1) not in rocks and (
+                sand[0] - 1,
+                sand[1] + 1,
+            ) not in sands:
+                sand = (sand[0] - 1, sand[1] + 1)
+            elif (sand[0] + 1, sand[1] + 1) not in rocks and (
+                sand[0] + 1,
+                sand[1] + 1,
+            ) not in sands:
+                sand = (sand[0] + 1, sand[1] + 1)
+            else:
+                break
+        if sand == (500, 0):
+            return count + 1
+        sands.add(sand)
+
+    return 1
 
 
 def solve(puzzle_input):
